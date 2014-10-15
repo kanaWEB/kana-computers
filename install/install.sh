43cfe2016daf08c0c4f5c637e3c6063d7a591191
madnerds <maditnerd@gmail.com>
2014-10-15 08:08:31 +0000

    Add install scripts

---
+++ install/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get -f install samba-common-bin ethtool
+apt-get -f install sshpass
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR
+cd ..
+chown root:www-data ./actions/switchvnc/bin/noVNC/utils/websockify
+chmod +sx ./actions/switchvnc/bin/noVNC/utils/websockify
