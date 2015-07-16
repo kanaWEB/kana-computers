5b5229e7d81f5183812177d4b354e2c98d47079f
maditnerd <sarrailh.remi@gmail.com>
2015-07-16 14:58:05 +0200

    Install script adapt to piget

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-apt-get -f install samba-common-bin ethtool
-apt-get -f install sshpass
-apt-get -f install wakeonlan
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $DIR
-cd ..
-chown root:www-data ./actions/switchvnc/bin/noVNC/utils/websockify
-chmod +sx ./actions/switchvnc/bin/noVNC/utils/websockify
+. /opt/piget/core/install/extendPath
+downloadModule computers
+. extendPath
+computersInstall
+downloadModule noVNC
+. extendPath
+noVNCInstall
+nginxAuthorize /opt/piget/computers/computersOff
+nginxAuthorize /opt/piget/computers/computersOn
+
