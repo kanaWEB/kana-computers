95f2b55be2b704af68fa013d36e851c3784fcb47
maditnerd <sarrailh.remi@gmail.com>
2015-10-01 17:41:38 +0200

    Checked install scripts

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-. /opt/piget/core/install/extendPath
-downloadModule computers
-. extendPath
-computersInstall
-downloadModule noVNC
-. extendPath
-noVNCInstall
-nginxAuthorize /opt/piget/computers/computersOff
-nginxAuthorize /opt/piget/computers/computersOn
+/system/downloadModule computers
+/system/computers/install
+/system/downloadModule noVNC
+/do/noVNC/install
+/do/nginx/authorize /do/computers/off
+/do/nginx/authorize /do/computers/on
 
